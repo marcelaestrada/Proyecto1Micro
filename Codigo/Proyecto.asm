e380b5a0289715eec3aef7352797c932cad05312
Marcela Estrada <marcelaestradarl@gmail.com>
2021-10-08 01:48:12 -0600

    Added the four cases, missing testing and printing methods

--- Codigo/Proyecto.asm
+++ Codigo/Proyecto.asm
@@ -11,72 +11,81 @@ decenas DB ?
 unidades DB ? 
 total DB ?
 ;Variables para las coordenadas y contador
+cInicial DB 01h
+nActual DB 00h
+coordenadaX DB 00h
+coordenadaY DB 00h
 .stack
 .code
 
 ;Iniciar programa
 
 	programa:
-	MOV AX, @DATA
-	MOV DS, AX
+		MOV AX, @DATA
+		MOV DS, AX
 
-	;imprimir ejercicio y leer primer ejercicio
-	MOV DX, offset proyecto
-	MOV AH, 09h
-	INT 21h
+		;imprimir ejercicio y leer primer ejercicio
+		MOV DX, offset proyecto
+		MOV AH, 09h
+		INT 21h
 
-	;Enter
-	MOV DL, 0Ah
-	MOV AH, 02h
-	INT 21h
-	
-	MOV DX, offset pedirNum
-	MOV AH, 09h
-	INT 21h
-	
-	;Enter
-	MOV DL, 0Ah
-	MOV AH, 02h
-	INT 21h
+		;Enter
+		MOV DL, 0Ah
+		MOV AH, 02h
+		INT 21h
+		
+		MOV DX, offset pedirNum
+		MOV AH, 09h
+		INT 21h
+		
+		;Enter
+		MOV DL, 0Ah
+		MOV AH, 02h
+		INT 21h
 
-	;leer el numero
-	MOV AH, 01h
-	INT 21h
-	SUB AL, 30h
-	MOV centenas, AL
-	CALL sonNumeros
+		;leer el numero
+		MOV AH, 01h
+		INT 21h
+		SUB AL, 30h
+		MOV centenas, AL
+		CALL sonNumeros
 
-	MOV AH, 01h
-	INT 21h
-	SUB AL, 30h
-	MOV decenas, AL
-	CALL sonNumeros
+		MOV AH, 01h
+		INT 21h
+		SUB AL, 30h
+		MOV decenas, AL
+		CALL sonNumeros
 
-	MOV AH, 01h
-	INT 21h
-	SUB AL, 30h
-	MOV unidades, AL
-	CALL sonNumeros
-	
-	CALL primeraValidacion
+		MOV AH, 01h
+		INT 21h
+		SUB AL, 30h
+		MOV unidades, AL
+		CALL sonNumeros
+		
+		CALL primeraValidacion
 	
 	;UNIR LOS VALORES PARA FORMAR UN NÚMERO TOTAL
 	;Multiplicación de las centenas por cien, decenas por diez y unidades como están, sumarlo todo
 	formarNumero:
-	XOR AX,AX
-	MOV AL, centenas
-	MUL 64h
-	MOV total, AL
+		XOR AX,AX
+		MOV AL, centenas
+		MUL 64h
+		MOV total, AL
 	
-	XOR AX,AX
-	MOV AL, decenas
-	MUL 0Ah
-	ADD total, AL
+		XOR AX,AX
+		MOV AL, decenas
+		MUL 0Ah
+		ADD total, AL
 	
-	MOV AL, unidades
-	ADD total, AL
+		MOV AL, unidades
+		ADD total, AL
+	
+	;Llamar al primer caso
+	llamarCasos:
+		CALL primerCaso
 	
 	;VALIDACIÓN DE LOS NÚMEROS
+	;Valida que sean caracteres validos entre 0 y 9
 	sonNumeros PROC NEAR
 		CMP AL, 0h
 		JL mostrarError
@@ -123,6 +132,131 @@ total DB ?
 		INT 21h
 		JMP Fin
 	mostrarError ENDP
+	
+	;PROCEDIMIENTOS PARA ENCONTRAR LAS COORDENADAS
+	;Primer caso: numeros iguales con signo positivo
+	primerCaso PROC NEAR
+		XOR AX,AX
+		MOV AL, coordenadaX
+		SUB 01h
+		MOV coordenadaX, AL
+		
+		CALL imprimirActual
+		CMP total, 01h
+		JL Fin
+		
+		;Verificacion si sigue en el mismo caso
+		MOV AL, cInicial
+		NEG AL
+		TEST coordenadaX, AL
+		JS segundoCaso
+		CALL primerCaso
+	primerCaso ENDP
+	
+	;Segundo caso: numeros iguales con signo negativo-positivo
+	segundoCaso PROC NEAR
+		XOR AX,AX
+		MOV AL, coordenadaY
+		SUB 01h
+		MOV coordenadaY, AL
+		
+		CALL imprimirActual
+		CMP total, 01h
+		JL Fin
+		
+		;Verificacion si sigue en el mismo caso
+		MOV AL, cInicial
+		NEG AL
+		TEST coordenadaY, AL
+		JS tercerCaso
+		CALL segundoCaso
+	segundoCaso ENDP
+	
+	;Tercer caso: numeros iguales con signo negativo-negativo
+	tercerCaso PROC NEAR
+		XOR AX,AX
+		MOV AL, coordenadaX
+		ADD 01h
+		MOV coordenadaX, AL
+		
+		CALL imprimirActual
+		CMP total, 01h
+		JL Fin
+		
+		;Verificacion si sigue en el mismo caso
+		TEST coordenadaX, cInicial
+		JS aumentarUnoX
+		CALL tercerCaso
+	tercerCaso ENDP
+	
+	;Cuarto caso: numeros iguales con signo positivo-negativo
+	cuartoCaso PROC NEAR
+		XOR AX,AX
+		MOV AL, coordenadaY
+		ADD 01h
+		MOV coordenadaY, AL
+		
+		CALL imprimirActual
+		CMP total, 01h
+		JL Fin
+		
+		;Verificacion si sigue en el mismo caso
+		TEST coordenadaY, coordenadaX
+		JS primerCaso
+		CALL cuartoCaso
+	cuartoCaso ENDP
+	
+	aumentarUnoX PROC NEAR 
+		XOR AX,AX
+		MOV AL, coordenadaX
+		ADD 01h
+		MOV coordenadaX, AL
+		
+		XOR AX,AX
+		MOV AL, cInicial
+		ADD 01h
+		MOV cInicial, AL
+		
+		CALL imprimirActual
+		JMP cuartoCaso
+	aumentarUnoX ENDP
+	
+	;IMPRESION NUMEROS Y COORDENADAS
+	;Impresion del numero actual
+	imprimirActual PROC NEAR
+		XOR AX,AX
+		MOV AL, total
+		SUB 01h
+		MOV total, AL
+		
+		XOR AX,AX
+		MOV AL, nActual
+		DIV 64h
+		
+		
+	imprimirActual ENDP
+	
+	;Impresion de la coordenada
+	imprimirCoordenada PROC NEAR
+		;Abrir parentesis
+		MOV DL, 28h
+		MOV AH, 02h
+		INT 21h
+		
+		;Coordenada x
+		
+		;Coma
+		MOV DL, 2Ch
+		MOV AH, 02h
+		INT 21h
+		
+		;Coordenada y 
+		
+		;Cerrar parentesis
+		MOV DL, 29h
+		MOV AH, 02h
+		INT 21h
+	imprimirCoordenada ENDP
 	
 	;Finalizar programa
 	Fin:
