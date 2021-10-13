2af9f528132e757b00d4383b47befda83a3a2e12
Marcela Estrada <marcelaestradarl@gmail.com>
2021-10-13 00:46:40 -0600

    Coordinates ready

--- Codigo/Proyecto.asm
+++ Codigo/Proyecto.asm
@@ -1,263 +1,544 @@
-.model small
+﻿.model small
 .data
-;Cadenas de texto
-proyecto DB 'PROYECTO 1$'
-pedirNum DB 'Ingrese un número entre 1 y 100 en el siguiente formato: ###$'
-errorNum DB 'Este número está fuera del rango aceptado (1-100)$'
-coordenada DB 'Los números solicitados y coordenadas son: $'
-;Numeros para leer y el total
+iproyecto DB 'PROYECTO 1: Ingrese un numero entre 1 y 100, siguiendo el formato ###$'
+errorNum DB 'Este numero no es valido$'
+pruebaNum DB 'El numero es valido$'
+primera DB '(0,0)$'
+segunda DB '(1,0)$'
+tercera DB '(1,1)$'
+coordenadaX DB 01h
+coordenadaY DB 01h
+bandera DB 01h
+resi DB ?
+diez DB 0Ah
 centenas DB ?
 decenas DB ?
 unidades DB ? 
 total DB ?
-;Variables para las coordenadas y contador
-cInicial DB 01h
-nActual DB 00h
-coordenadaX DB 00h
-coordenadaY DB 00h
 .stack
 .code
 
 ;Iniciar programa
 
 	programa:
-		MOV AX, @DATA
-		MOV DS, AX
+	MOV AX, @DATA
+	MOV DS, AX
 
-		;imprimir ejercicio y leer primer ejercicio
-		MOV DX, offset proyecto
-		MOV AH, 09h
-		INT 21h
+	;imprimir ejercicio y leer primer ejercicio
+	MOV DX, offset iproyecto
+	MOV AH, 09h
+	INT 21h
 
-		;Enter
-		MOV DL, 0Ah
-		MOV AH, 02h
-		INT 21h
-		
-		MOV DX, offset pedirNum
-		MOV AH, 09h
-		INT 21h
-		
-		;Enter
-		MOV DL, 0Ah
-		MOV AH, 02h
-		INT 21h
+	;Enter
+	MOV DL, 0Ah
+	MOV AH, 02h
+	INT 21h
 
-		;leer el numero
-		MOV AH, 01h
-		INT 21h
-		SUB AL, 30h
-		MOV centenas, AL
-		CALL sonNumeros
+	;leer el numero
+	MOV AH, 01h
+	INT 21h
+	SUB AL, 30h
+	MOV centenas, AL
+	CALL sonNumeros
 
-		MOV AH, 01h
-		INT 21h
-		SUB AL, 30h
-		MOV decenas, AL
-		CALL sonNumeros
+	MOV AH, 01h
+	INT 21h
+	SUB AL, 30h
+	MOV decenas, AL
+	CALL sonNumeros
 
-		MOV AH, 01h
-		INT 21h
-		SUB AL, 30h
-		MOV unidades, AL
-		CALL sonNumeros
-		
-		CALL primeraValidacion
+	MOV AH, 01h
+	INT 21h
+	SUB AL, 30h
+	MOV unidades, AL
+	CALL sonNumeros
 	
-	;UNIR LOS VALORES PARA FORMAR UN NÚMERO TOTAL
-	;Multiplicación de las centenas por cien, decenas por diez y unidades como están, sumarlo todo
+	CALL primeraValidacion
+	
+	;Formar el número
 	formarNumero:
-		XOR AX,AX
-		MOV AL, centenas
-		MUL 64h
-		MOV total, AL
+	XOR AX,AX
+	MOV AL, 64h
+	MUL centenas
+	MOV total, AL
 	
-		XOR AX,AX
-		MOV AL, decenas
-		MUL 0Ah
-		ADD total, AL
+	XOR AX,AX
+	MOV AL, 0Ah
+	MUL decenas
+	ADD total, AL
 	
-		MOV AL, unidades
-		ADD total, AL
+	MOV AL, unidades
+	ADD total, AL
+
+	CMP total, 00h
+	JG llamarCasos
+	JMP llamarFinal		
 	
-	;Llamar al primer caso
-	llamarCasos:
-		CALL primerCaso
+	;LLAMAR CASOS
+	llamarCasos proc near
+	CALL imprimirEnter
 	
-	;VALIDACIÓN DE LOS NÚMEROS
-	;Valida que sean caracteres validos entre 0 y 9
-	sonNumeros PROC NEAR
-		CMP AL, 0h
-		JL mostrarError
-		CMP AL, 09h
-		JG mostrarError
-	ret
-	sonNumeros ENDP
+	;imprimir primera coordenada
+	MOV DX, offset primera
+	MOV AH, 09h
+	INT 21h
 	
-	;Primero ver si las centenas es 0 o 1
-	primeraValidacion PROC NEAR
-		CMP centenas, 01h
-		JE validacionCien
-		CMP decenas, 01h
-		JNE validacionUnidades
-		JGE formarNumero
-		JMP mostrarError
-	primeraValidacion ENDP
+	CALL restaTotal
+	JLE llamarFinal
+	CALL imprimirEnter
+
+	;imprimir segunda coordenada
+	MOV DX, offset segunda
+	MOV AH, 09h
+	INT 21h
+
+	CALL restaTotal
+	JLE llamarFinal
+	CALL imprimirEnter
 	
-	;Validación en el caso que las centenas sea 1, las decenas y unidades deben ser 0
-	validacionCien PROC NEAR
-		CMP decenas, 01h
-		JGE mostrarError
-		CMP unidades, 01h
-		JGE mostrarError
-		JMP formarNumero
-	validacionCien ENDP
+	;imprimir tercera coordenada
+	MOV DX, offset tercera
+	MOV AH, 09h
+	INT 21h
+
+	CALL restaTotal
+	JLE llamarFinal	
+	JMP primerCasoResta
 	
-	;Validacion que no ingrese 000, no seria valido
-	validacionUnidades PROC NEAR
-		CMP unidades, 01h
-		JL mostrarError
-		JMP formarNumero
-	validacionUnidades ENDP
+	llamarCasos endp	
 	
-	;Mostrar número ingresado no es correcto
-	mostrarError PROC NEAR
-		;Enter
+	;ENTER
+	imprimirEnter proc near
 		MOV DL, 0Ah
 		MOV AH, 02h
 		INT 21h
+	ret
+	imprimirEnter endp	
 	
-		MOV DX, offset errorNum
-		MOV AH, 09h
-		INT 21h
+	;RESTAR
+	restaTotal proc near
+		MOV BH, total
+		SUB BH, 01h
+		MOV total, BH
+	ret
+	restaTotal endp
+
+	;LLAMAR FINAL	
+	llamarFinal:
 		JMP Fin
-	mostrarError ENDP
-	
-	;PROCEDIMIENTOS PARA ENCONTRAR LAS COORDENADAS
-	;Primer caso: numeros iguales con signo positivo
-	primerCaso PROC NEAR
+
+	;PRIMER CASO
+	primerCasoResta proc near
 		XOR AX,AX
 		MOV AL, coordenadaX
-		SUB 01h
-		MOV coordenadaX, AL
+		SUB AL, 01h
+		DIV diez
+		MOV coordenadaX, AH
 		
-		CALL imprimirActual
-		CMP total, 01h
-		JL Fin
+		CALL imprimirCoordenada
+		CALL restaTotal
+		JLE llamarFinal
 		
-		;Verificacion si sigue en el mismo caso
-		MOV AL, cInicial
-		NEG AL
-		TEST coordenadaX, AL
-		JS segundoCaso
-		CALL primerCaso
-	primerCaso ENDP
+		CMP coordenadaX, 01h
+		JL primerCasoSuma
+		JMP primerCasoResta
+	primerCasoResta endp	
 	
-	;Segundo caso: numeros iguales con signo negativo-positivo
-	segundoCaso PROC NEAR
+	primerCasoSuma proc near
+		XOR AX,AX
+		MOV AL, coordenadaX
+		ADD AL, 01h
+		DIV diez
+		MOV coordenadaX, AH
+
+		CALL imprimirCoordenadaN
+		CALL restaTotal
+		JLE llamarFinal
+
+		XOR AX, AX
+		MOV AL, bandera
+		CMP coordenadaX, AL
+		JE segundoCasoResta
+		JMP primerCasoSuma
+	primerCasoSuma endp	
+
+	segundoCasoResta proc near
 		XOR AX,AX
 		MOV AL, coordenadaY
-		SUB 01h
-		MOV coordenadaY, AL
+		SUB AL, 01h
+		DIV diez
+		MOV coordenadaY, AH
 		
-		CALL imprimirActual
-		CMP total, 01h
-		JL Fin
+		CALL imprimirCoordenadaN
+		CALL restaTotal
+		JLE llamarFinal
 		
-		;Verificacion si sigue en el mismo caso
-		MOV AL, cInicial
-		NEG AL
-		TEST coordenadaY, AL
-		JS tercerCaso
-		CALL segundoCaso
-	segundoCaso ENDP
+		CMP coordenadaY, 01h
+		JL segundoCasoSuma
+		JMP segundoCasoResta
+	segundoCasoResta endp
 	
-	;Tercer caso: numeros iguales con signo negativo-negativo
-	tercerCaso PROC NEAR
-		XOR AX,AX
-		MOV AL, coordenadaX
-		ADD 01h
-		MOV coordenadaX, AL
-		
-		CALL imprimirActual
-		CMP total, 01h
-		JL Fin
-		
-		;Verificacion si sigue en el mismo caso
-		TEST coordenadaX, cInicial
-		JS aumentarUnoX
-		CALL tercerCaso
-	tercerCaso ENDP
-	
-	;Cuarto caso: numeros iguales con signo positivo-negativo
-	cuartoCaso PROC NEAR
+	lPrimerCasoResta:
+		JMP primerCasoResta
+
+	segundoCasoSuma proc near
 		XOR AX,AX
 		MOV AL, coordenadaY
-		ADD 01h
-		MOV coordenadaY, AL
-		
-		CALL imprimirActual
-		CMP total, 01h
-		JL Fin
-		
-		;Verificacion si sigue en el mismo caso
-		TEST coordenadaY, coordenadaX
-		JS primerCaso
-		CALL cuartoCaso
-	cuartoCaso ENDP
+		ADD AL, 01h
+		DIV diez
+		MOV coordenadaY, AH
+
+		CALL imprimirCoordenadaNN
+		CALL restaTotal
+		JLE llamarFinal
+
+		XOR AX, AX
+		MOV AL, bandera
+		CMP coordenadaY, AL
+		JE tercerCasoResta
+		JMP segundoCasoSuma
+	segundoCasoSuma endp	
 	
-	aumentarUnoX PROC NEAR 
+	tercerCasoResta proc near
 		XOR AX,AX
 		MOV AL, coordenadaX
-		ADD 01h
-		MOV coordenadaX, AL
+		SUB AL, 01h
+		DIV diez
+		MOV coordenadaX, AH
 		
+		CALL llamarImpresion
+		CALL restaTotal
+		JLE llamarFin2
+		
+		CMP coordenadaX, 01h
+		JL tercerCasoSuma
+		JMP tercerCasoResta
+	tercerCasoResta endp	
+
+	tercerCasoSuma proc near
 		XOR AX,AX
-		MOV AL, cInicial
-		ADD 01h
-		MOV cInicial, AL
+		MOV AL, coordenadaX
+		ADD AL, 01h
+		DIV diez
+		MOV coordenadaX, AH
 		
-		CALL imprimirActual
-		JMP cuartoCaso
-	aumentarUnoX ENDP
+		CALL imprimirCoordenadaaN
+		CALL restaTotal
+		JLE llamarFin2
+		
+		XOR AX, AX
+		MOV AL, bandera
+		CMP coordenadaX, AL
+		JE aumentarBandera
+		JMP tercerCasoSuma
+	tercerCasoSuma endp	
+
+	llamarFin2:
+		JMP Fin	
+
+	aumentarBandera proc near
+		XOR AX,AX
+		MOV AL, bandera
+		ADD AL, 01h
+		DIV diez
+		MOV bandera, AH
+		MOV coordenadaX, AH
+
+		CALL imprimirCoordenadaaN
+		CALL restaTotal
+		JLE llamarFin4
+		
+		CMP bandera, 02h
+		JE intermedio
+		JMP cuartoCasoResta
+	aumentarBandera endp
 	
-	;IMPRESION NUMEROS Y COORDENADAS
-	;Impresion del numero actual
-	imprimirActual PROC NEAR
+	intermedio proc near
 		XOR AX,AX
-		MOV AL, total
-		SUB 01h
-		MOV total, AL
+		MOV AL, coordenadaY
+		SUB AL, 01h
+		DIV diez
+		MOV coordenadaY, AH
 		
+		CALL imprimirCoordenada
+		CALL restaTotal
+		JLE llamarFin4
+		JMP cuartoCasoSuma
+	intermedio endp
+
+	llamarPrimerCasoResta:
+		JMP lPrimerCasoResta
+
+	cuartoCasoResta proc near
 		XOR AX,AX
-		MOV AL, nActual
-		DIV 64h
+		MOV AL, coordenadaY
+		SUB AL, 01h
+		DIV diez
+		MOV coordenadaY, AH
 		
+		CALL llamarImprimirCoor
+		CALL restaTotal
+		JLE llamarFin4
+
+		CMP coordenadaY, 01h
+		JE intermedio
+		JMP cuartoCasoResta
+	cuartoCasoResta endp
+
+	cuartoCasoSuma proc near
+		XOR AX,AX
+		MOV AL, coordenadaY
+		ADD AL, 01h
+		DIV diez
+		MOV coordenadaY, AH
 		
-	imprimirActual ENDP
-	
-	;Impresion de la coordenada
-	imprimirCoordenada PROC NEAR
-		;Abrir parentesis
+		CALL imprimirCoordenada
+		CALL restaTotal
+		JLE llamarFin4
+		
+		XOR AX, AX
+		MOV AL, bandera
+		CMP coordenadaY, AL
+		JE llamarPrimerCasoResta
+		JMP cuartoCasoSuma
+	cuartoCasoSuma endp		
+
+	llamarFin4:
+		JMP Fin
+
+	;IMPRIMIR NUMEROS Y PARENTESIS
+	imprimirCoordenada proc near 
+		CALL imprimirEnter
+		;parentesis
+		XOR DX, DX
 		MOV DL, 28h
 		MOV AH, 02h
 		INT 21h
 		
-		;Coordenada x
-		
+		XOR DX, DX
+		MOV DL, coordenadaX
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
 		;Coma
+		XOR DX, DX
 		MOV DL, 2Ch
 		MOV AH, 02h
 		INT 21h
 		
-		;Coordenada y 
-		
-		;Cerrar parentesis
+		XOR DX, DX
+		MOV DL, coordenadaY
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;parentesis
+		XOR DX, DX
 		MOV DL, 29h
 		MOV AH, 02h
 		INT 21h
-	imprimirCoordenada ENDP
+	ret
+	imprimirCoordenada endp
+
+	llamarFin3:
+		JMP Fin	
+
+	llamarImpresion proc near
+		CMP coordenadaY, 01h
+		JLE llamarImpresion2
+		JG imprimirCoordenadaNN
+	ret
+	llamarImpresion endp
+
+	llamarImprimirCoor proc near
+		CALL imprimirCoordenadaaN
+	ret
+	llamarImprimirCoor endp	
+
+	imprimirCoordenadaN proc near 
+		CALL imprimirEnter
+		;parentesis
+		XOR DX, DX
+		MOV DL, 28h
+		MOV AH, 02h
+		INT 21h
+
+		;menos
+		XOR DX, DX
+		MOV DL, 2Dh
+		MOV AH, 02h
+		INT 21h	
+
+		XOR DX, DX
+		MOV DL, coordenadaX
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;Coma
+		XOR DX, DX
+		MOV DL, 2Ch
+		MOV AH, 02h
+		INT 21h
+		
+		XOR DX, DX
+		MOV DL, coordenadaY
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;parentesis
+		XOR DX, DX
+		MOV DL, 29h
+		MOV AH, 02h
+		INT 21h
+	ret
+	imprimirCoordenadaN endp
 	
+	llamarImpresion2 proc near
+		JMP imprimirCoordenadaaN
+	ret
+	llamarImpresion2 endp
+	
+	imprimirCoordenadaNN proc near 
+		CALL imprimirEnter
+		;parentesis
+		XOR DX, DX
+		MOV DL, 28h
+		MOV AH, 02h
+		INT 21h
+
+		;menos
+		XOR DX, DX
+		MOV DL, 2Dh
+		MOV AH, 02h
+		INT 21h	
+
+		XOR DX, DX
+		MOV DL, coordenadaX
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;Coma
+		XOR DX, DX
+		MOV DL, 2Ch
+		MOV AH, 02h
+		INT 21h
+		
+		;menos
+		XOR DX, DX
+		MOV DL, 2Dh
+		MOV AH, 02h
+		INT 21h
+
+		XOR DX, DX
+		MOV DL, coordenadaY
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;parentesis
+		XOR DX, DX
+		MOV DL, 29h
+		MOV AH, 02h
+		INT 21h
+	ret
+	imprimirCoordenadaNN endp
+
+	imprimirCoordenadaaN proc near 
+		CALL imprimirEnter
+		;parentesis
+		XOR DX, DX
+		MOV DL, 28h
+		MOV AH, 02h
+		INT 21h
+
+		XOR DX, DX
+		MOV DL, coordenadaX
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;Coma
+		XOR DX, DX
+		MOV DL, 2Ch
+		MOV AH, 02h
+		INT 21h
+		
+		;menos
+		XOR DX, DX
+		MOV DL, 2Dh
+		MOV AH, 02h
+		INT 21h
+
+		XOR DX, DX
+		MOV DL, coordenadaY
+		ADD DL, 30h
+		MOV AH, 02h
+		INT 21h
+
+		;parentesis
+		XOR DX, DX
+		MOV DL, 29h
+		MOV AH, 02h
+		INT 21h
+	ret
+	imprimirCoordenadaaN endp
+
+	;LLAMAR FORMAR NUMERO
+	llamarFormarNumero:
+		JMP formarNumero
+
+	;VALIDACIÓN DE LOS NÚMEROS
+	sonNumeros proc near
+		CMP AL, 00h
+		JL mostrarError
+		CMP AL, 09h
+		JG mostrarError
+	ret
+	sonNumeros endp
+	
+	;Primero ver si las centenas es 0 o 1
+	primeraValidacion proc near
+		CMP centenas, 01h
+		JE validacionCien
+		CMP decenas, 01h
+		JL validacionUnidades
+		JGE llamarFormarNumero
+		JMP mostrarError
+	primeraValidacion endp
+	
+	;Validación en el caso que las centenas sea 1, las decenas y unidades deben ser 0
+	validacionCien proc near
+		CMP decenas, 01h
+		JGE mostrarError
+		CMP unidades, 01h
+		JGE mostrarError
+		JMP formarNumero
+	validacionCien endp
+
+	;Validación en el caso que las centenas y decenas sean 0, unidades tiene que ser mínimo 1
+	validacionUnidades proc near
+		CMP unidades, 01h
+		JL mostrarError
+		JMP formarNumero
+	validacionUnidades endp
+
+	;Mostrar número ingresado no es correcto
+	mostrarError proc near
+		;Enter
+		MOV DL, 0Ah
+		MOV AH, 02h
+		INT 21h
+
+		MOV DX, offset errorNum
+		MOV AH, 09h
+		INT 21h
+		JMP Fin
+	mostrarError endp
+
 	;Finalizar programa
 	Fin:
 		MOV AH,4CH			    
